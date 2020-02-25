9a27ca39f311aecf2a9bf227aff4a197e2a4f563
lourencofelipe <felipe.pucsc@gmail.com>
2020-02-25 08:32:26 -0300

    Creating server

---
+++ server.js
@@ -0,0 +1,8 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+connect()
+.use(serveStatic(__dirname))
+.listen(8080, function() {
+    console.log('Server running on 8080...');
+    console.log('To stop the server: ctrl + c');
+});
